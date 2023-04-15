2154cadd22f7e3f71deb3ae8a75b5c513a93336f
abesek <86023984+abesek@users.noreply.github.com>
2023-04-15 23:30:35 +0530

    add factors

---
+++ Factors.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factors {
+    //all factors are <= n/2
+    //square root of n will be in middle of factors list
+    //negative factors??
+    static void getFactors(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.printf("(%d, %d)\n", i, n / i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        sc.close();
+        getFactors(n);
+    }
+}
