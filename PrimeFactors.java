801c9ade0d6efd668c23f5f1d48f5091af3735f4
abesek <86023984+abesek@users.noreply.github.com>
2023-04-17 23:18:36 +0530

    add largest prime factor

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -2,19 +2,26 @@ import java.util.Scanner;
 
 public class PrimeFactors {
     static void getFactors(int n) {
-        //remove all 2 factors
+        int largestPrimeFactor = -1;
+        //remove all factors of 2
         while (n % 2 == 0) {
             System.out.println(2);
             n /= 2;
+            largestPrimeFactor = 2;
         }
         //n is odd by this point
         for (int i = 3; i <= Math.sqrt(n); i += 2) { //skip even numbers
             while (n % i == 0) {
                 System.out.println(i);
                 n /= i;
+                largestPrimeFactor = i;
             }
         }
-        if (n > 1) System.out.println(n); 
+        if (n > 1) { 
+            System.out.println(n); 
+            largestPrimeFactor = n; 
+        }
+        System.out.println("Largest Prime Factor = " + largestPrimeFactor);
     }
 
     public static void main(String[] args) {
